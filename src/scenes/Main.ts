9dfa7f486387fa1a04417eac08b13fc3db01043c
Mark <mark@remarkablemark.org>
2022-01-09 21:59:15 -0500

    feat(scenes): add world bounds and camera zoom and follow player

--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -13,8 +13,17 @@ export default class Main extends Phaser.Scene {
   create() {
     const map = this.make.tilemap({ key: Tilemap.Desert });
     const tiles = map.addTilesetImage(Tileset.Desert, Image.Desert);
-    map.createLayer(Layer.Ground, tiles, 0, 0);
+    const layer = map.createLayer(Layer.Ground, tiles, 0, 0);
+
+    this.physics.world.bounds.width = layer.width;
+    this.physics.world.bounds.height = layer.height;
+
     player = new Player(this, 32, Number(this.game.config.height) - 150);
+    player.setCollideWorldBounds(true);
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(player);
+    this.cameras.main.setZoom(2);
   }
 
   update(time: number, delta: number) {
