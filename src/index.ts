e2af08f303b67f755f54ded1cbc7d6c62102e6af
Mark <mark@remarkablemark.org>
2022-01-29 19:47:46 -0500

    refactor(scenes): tidy scenes export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './style.css';
 
 import Phaser from 'phaser';
 
-import * as scenes from './scenes';
+import scenes from './scenes';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -10,13 +10,13 @@ const isProduction = process.env.NODE_ENV === 'production';
  * https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
  */
 new Phaser.Game({
-  width: 800,
-  height: 600,
+  width: 800, // 1024
+  height: 600, // 768
   zoom: 2,
   title: 'Phaser RPG',
   url: 'https://remarkablegames.org/phaser-rpg/',
   version: process.env.VERSION,
-  scene: Object.values(scenes),
+  scene: scenes,
   physics: {
     default: 'arcade',
     arcade: {
