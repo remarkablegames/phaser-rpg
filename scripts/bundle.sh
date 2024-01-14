94be3346de5cdf51a1f2aa888d7d931fc3cfecc7
Mark <mark@remarkablemark.org>
2024-01-14 16:07:41 -0500

    build(scripts): add bundle script

---
+++ scripts/bundle.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+BUNDLE=true npm run build
+
+# replace absolute URLs with relative URLs
+if [ "$(uname)" == 'Darwin' ]; then
+  sed -i '' -e 's|src="/|src="|g' -e 's|href="/|href="|g' dist/index.html
+else
+  sed -i -e 's|src="/|src="|g' -e 's|href="/|href="|g' dist/index.html
+fi
+
+# zip file
+zip -r "dist/$(npm pkg get name | tr -d \")-$(npm pkg get version | tr -d \").zip" dist
+echo
+find dist -type f -depth 1 -name '*.zip'
+open dist
