130037cc7db24dbc88b1e994da965bfca3f228d8
Mark <mark@remarkablemark.org>
2024-03-30 20:55:24 -0400

    refactor(constants): create constants for tilemap name, layer, object

---
+++ src/constants/tilemap.ts
@@ -0,0 +1,12 @@
+export const TILESET_NAME = 'tuxemon-sample-32px-extruded';
+
+export enum TilemapLayer {
+  BelowPlayer = 'Below Player',
+  World = 'World',
+  AbovePlayer = 'Above Player',
+  Objects = 'Objects',
+}
+
+export enum TilemapObject {
+  SpawnPoint = 'Spawn Point',
+}
--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -1,6 +1,11 @@
 import Phaser from 'phaser';
 
-import { isDevelopment } from '../constants';
+import {
+  isDevelopment,
+  TilemapLayer,
+  TilemapObject,
+  TILESET_NAME,
+} from '../constants';
 import { key } from '../data';
 import { Player } from '../sprites';
 
@@ -17,15 +22,17 @@ export default class Main extends Phaser.Scene {
 
     // Parameters are the name you gave the tileset in Tiled and
     // the key of the tileset image in Phaser's cache (name used in preload)
-    const tileset = map.addTilesetImage(
-      'tuxemon-sample-32px-extruded',
-      key.image.tuxemon,
-    )!;
+    const tileset = map.addTilesetImage(TILESET_NAME, key.image.tuxemon)!;
 
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    map.createLayer('Below Player', tileset, 0, 0);
-    const worldLayer = map.createLayer('World', tileset, 0, 0)!;
-    const aboveLayer = map.createLayer('Above Player', tileset, 0, 0)!;
+    map.createLayer(TilemapLayer.BelowPlayer, tileset, 0, 0);
+    const worldLayer = map.createLayer(TilemapLayer.World, tileset, 0, 0)!;
+    const aboveLayer = map.createLayer(
+      TilemapLayer.AbovePlayer,
+      tileset,
+      0,
+      0,
+    )!;
 
     worldLayer.setCollisionByProperty({ collides: true });
     this.physics.world.bounds.width = worldLayer.width;
@@ -39,8 +46,8 @@ export default class Main extends Phaser.Scene {
     // Object layers in Tiled let you embed extra info into a map like a spawn point or custom collision shapes.
     // In the tmx file, there's an object layer with a point named 'Spawn Point'.
     const spawnPoint = map.findObject(
-      'Objects',
-      (object) => object.name === 'Spawn Point',
+      TilemapLayer.Objects,
+      (object) => object.name === TilemapObject.SpawnPoint,
     )!;
 
     this.player = new Player(this, spawnPoint.x || 0, spawnPoint.y || 0);
