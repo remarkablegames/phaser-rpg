3f7d582881321cfc83723ecc20d0d92e168460cf
Mark <mark@remarkablemark.org>
2024-04-08 22:38:49 -0400

    refactor(components): tidy Overlay and depth constant

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,19 +1,17 @@
 import { Rectangle, useScene } from 'phaser-jsx';
 
-import { Depth } from '../constants';
-
 export function Overlay() {
   const scene = useScene();
+  const { width, height } = scene.cameras.main;
 
   return (
     <Rectangle
-      width={scene.cameras.main.width * 2}
-      height={scene.cameras.main.height * 2}
+      width={width * 2}
+      height={height * 2}
       fillColor={0x000000}
       alpha={0.5}
       scrollFactorX={0}
       scrollFactorY={0}
-      depth={Depth.Overlay}
     />
   );
 }
--- src/constants/depth.ts
+++ src/constants/depth.ts
@@ -1,5 +1,4 @@
 export enum Depth {
   AbovePlayer = 10,
   AboveWorld = 20,
-  Overlay = 30,
 }
