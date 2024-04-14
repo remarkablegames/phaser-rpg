72e6c688c78e34f7cfbd9cfe376e0b267e5df7d1
Mark <mark@remarkablemark.org>
2024-04-14 16:27:23 -0400

    refactor(scenes): tidy Main

--- src/scenes/Main.tsx
+++ src/scenes/Main.tsx
@@ -19,68 +19,58 @@ interface Sign extends Phaser.Physics.Arcade.StaticBody {
 export class Main extends Phaser.Scene {
   private player!: Player;
   private sign!: Sign;
+  private tilemap!: Phaser.Tilemaps.Tilemap;
+  private worldLayer!: Phaser.Tilemaps.TilemapLayer;
 
   constructor() {
     super(key.scene.main);
   }
 
   create() {
-    const map = this.make.tilemap({ key: key.tilemap.tuxemon });
+    this.tilemap = this.make.tilemap({ key: key.tilemap.tuxemon });
 
     // Parameters are the name you gave the tileset in Tiled and
     // the key of the tileset image in Phaser's cache (name used in preload)
-    const tileset = map.addTilesetImage(TILESET_NAME, key.image.tuxemon)!;
+    const tileset = this.tilemap.addTilesetImage(
+      TILESET_NAME,
+      key.image.tuxemon,
+    )!;
 
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    map.createLayer(TilemapLayer.BelowPlayer, tileset, 0, 0);
-    const worldLayer = map.createLayer(TilemapLayer.World, tileset, 0, 0)!;
-    const aboveLayer = map.createLayer(
+    this.tilemap.createLayer(TilemapLayer.BelowPlayer, tileset, 0, 0);
+    this.worldLayer = this.tilemap.createLayer(
+      TilemapLayer.World,
+      tileset,
+      0,
+      0,
+    )!;
+    const aboveLayer = this.tilemap.createLayer(
       TilemapLayer.AbovePlayer,
       tileset,
       0,
       0,
     )!;
 
-    worldLayer.setCollisionByProperty({ collides: true });
-    this.physics.world.bounds.width = worldLayer.width;
-    this.physics.world.bounds.height = worldLayer.height;
+    this.worldLayer.setCollisionByProperty({ collides: true });
+    this.physics.world.bounds.width = this.worldLayer.width;
+    this.physics.world.bounds.height = this.worldLayer.height;
 
     // By default, everything gets depth sorted on the screen in the order we created things.
     // We want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
     // Higher depths will sit on top of lower depth objects.
     aboveLayer.setDepth(Depth.AbovePlayer);
 
-    // Object layers in Tiled let you embed extra info into a map like a spawn point or custom collision shapes.
-    // In the tmx file, there's an object layer with a point named 'Spawn Point'.
-    const spawnPoint = map.findObject(
-      TilemapLayer.Objects,
-      ({ name }) => name === TilemapObject.SpawnPoint,
-    )!;
-
-    const sign = map.findObject(
-      TilemapLayer.Objects,
-      ({ name }) => name === TilemapObject.Sign,
-    )!;
-
-    this.sign = this.physics.add.staticBody(
-      sign.x!,
-      sign.y!,
-      sign.width,
-      sign.height,
-    );
-    this.sign.text = sign.properties[0].value;
-
-    this.player = new Player(this, spawnPoint.x!, spawnPoint.y!);
-
-    this.enablePlayerSignInteraction();
-
-    // Watch the player and worldLayer for collisions
-    this.physics.add.collider(this.player, worldLayer);
+    this.addPlayer();
 
     // Set the bounds of the camera
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.tilemap.widthInPixels,
+      this.tilemap.heightInPixels,
+    );
 
-    render(<TilemapDebug tilemapLayer={worldLayer} />, this);
+    render(<TilemapDebug tilemapLayer={this.worldLayer} />, this);
 
     state.isTypewriting = true;
     render(
@@ -97,7 +87,35 @@ export class Main extends Phaser.Scene {
     });
   }
 
-  private enablePlayerSignInteraction() {
+  private addPlayer() {
+    // Object layers in Tiled let you embed extra info into a map like a spawn point or custom collision shapes.
+    // In the tmx file, there's an object layer with a point named 'Spawn Point'.
+    const spawnPoint = this.tilemap.findObject(
+      TilemapLayer.Objects,
+      ({ name }) => name === TilemapObject.SpawnPoint,
+    )!;
+
+    this.player = new Player(this, spawnPoint.x!, spawnPoint.y!);
+    this.addPlayerSignInteraction();
+
+    // Watch the player and worldLayer for collisions
+    this.physics.add.collider(this.player, this.worldLayer);
+  }
+
+  private addPlayerSignInteraction() {
+    const sign = this.tilemap.findObject(
+      TilemapLayer.Objects,
+      ({ name }) => name === TilemapObject.Sign,
+    )!;
+
+    this.sign = this.physics.add.staticBody(
+      sign.x!,
+      sign.y!,
+      sign.width,
+      sign.height,
+    );
+    this.sign.text = sign.properties[0].value;
+
     type ArcadeColliderType = Phaser.Types.Physics.Arcade.ArcadeColliderType;
 
     this.physics.add.overlap(
@@ -116,7 +134,6 @@ export class Main extends Phaser.Scene {
           );
         }
       },
-
       undefined,
       this,
     );
