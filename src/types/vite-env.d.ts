10ab9cd04c3a7fa818e4fbc4900ee14edfa24afe
Mark <mark@remarkablemark.org>
2024-06-10 22:07:41 -0400

    build(env): set npm package variables in .env

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_HOMEPAGE: string;
+  readonly VITE_APP_NAME: string;
   readonly VITE_APP_VERSION: string;
   readonly VITE_GOOGLE_ANALYTICS_ID: string;
 }
